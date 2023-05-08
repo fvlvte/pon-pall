a56c04488f2ee177b129b151773b3b369261d5d3
Kacper 'fvlvte' Fałat <me@falat.me>
2023-05-08 16:47:00 -0600

    renamed internl project stuff

--- android/app/src/main/java/com/ponpall/MainApplication.java
+++ android/app/src/main/java/com/ponpall/MainApplication.java
@@ -1,4 +1,4 @@
-package com.spadajoncekuleprezesa;
+package com.ponpall;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/ponpallTests/ponpallTests.m
+++ ios/ponpallTests/ponpallTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SpadajonceKulePrezesaTests : XCTestCase
+@interface ponpallTests : XCTestCase
 
 @end
 
-@implementation SpadajonceKulePrezesaTests
+@implementation ponpallTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
